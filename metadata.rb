9a3bc22d931d668486c0869ca1c668f865d88904
Scott McLeod <smcleod@zeddworks.com>
2011-06-25 19:44:10 -0400

    Creating passenger_nginx cookbook

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "ZeddWorks"
+maintainer_email "smcleod@zeddworks.com"
+license          "Apache 2.0"
+description      "Installs/Configures passenger_nginx"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
---
+++ recipes/default.rb
@@ -0,0 +1,51 @@
+#
+# Cookbook Name:: passenger_nginx
+# Recipe:: default
+#
+# Copyright 2011, ZeddWorks
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+nginx_version="1.0.4"
+
+remote_file "/tmp/nginx-#{nginx_version}.tar.gz" do
+  source "http://nginx.org/download/nginx-#{nginx_version}.tar.gz"
+  action :create_if_missing
+  notifies :run, "execute[extract nginx]"
+end
+
+execute "extract nginx" do
+  command "tar zxvf nginx-#{nginx_version}.tar.gz"
+  cwd "/tmp"
+  action :nothing
+end
+
+gem_package "passenger"
+
+execute "compile-nginx" do
+  command "passenger-install-nginx-module --auto --prefix=/usr --nginx-source-dir=/tmp/nginx-#{nginx_version} --extra-configure-flags='--pid-path=/var/run/nginx.pid'"
+  not_if {File.exists?("/usr/sbin/nginx")}
+end
+
+cookbook_file "/etc/init.d/nginx" do
+  source "nginx-common.init.d"
+  mode "0755"
+  action :create_if_missing
+  notifies :run, "execute[update nginx rc scripts]"
+end
+
+execute "update nginx rc scripts" do
+  command "update-rc.d nginx defaults"
+  action :nothing
+end
